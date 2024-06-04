c765dc93cae04b2c798151e6adfd38078c755b16
CassDeLaRy <mauriciomtz893@gmail.com>
2024-06-03 23:54:39 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/api/v1/welcome', (req, res) => {
+    res.send('Welcome to the API');    
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
